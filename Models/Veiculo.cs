8827906c0ebac41250ab2d2f1fe644b08cc4691a
camlonghini <carolinalonghini@gmail.com>
2025-04-30 21:43:42 +0100

    first commit

---
+++ Models/Veiculo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace mf_backend.Models
+{
+    [Table("Veiculos")]
+	public class Veiculo
+	{
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigátorio informar o Nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigátorio informar a Placa")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "Obrigátorio informar o Ano de Fabricação")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "Obrigátorio informar o Ano do Modelo")]
+        public int AnoModelo { get; set; }
+    }
+}
+
