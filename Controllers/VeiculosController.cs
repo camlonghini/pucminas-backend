e7ee35c328857c90f67e46b212c55ed99e0373db
camlonghini <carolinalonghini@gmail.com>
2025-05-03 12:39:44 +0100

    Entity framework setup

---
+++ Controllers/VeiculosController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using mf_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace mf_backend.Controllers
+{
+	public class VeiculosController : Controller
+	{
+		private readonly AppDbContext _context;
+		public VeiculosController(AppDbContext context)
+		{
+			_context = context;
+
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var dados = await _context.Veiculos.ToListAsync();
+			return View(dados);
+		}
+
+	}
+}
+
